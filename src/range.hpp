4d2d9e866607b448293e9fad5b94a7bd239468f6
Thomas Mailund <mailund@birc.au.dk>
2023-01-24 15:12:41 +0100

    More hacking with traits and templates

--- src/range.hpp
+++ src/range.hpp
@@ -5,28 +5,36 @@
 
 namespace range {
 
-template <typename B, bool is_open> struct LowerBound;
-template <typename B, bool is_open> struct UpperBound;
+struct Open;
+struct Closed;
+template <typename> struct open_trait;
+template <> struct open_trait<Open> {
+  static constexpr bool value = true;
+};
+template <> struct open_trait<Closed> {
+  static constexpr bool value = false;
+};
+
+template <typename B, typename is_open> struct LowerBound;
+template <typename B, typename is_open> struct UpperBound;
+
+// clang-format off
+template <typename T, template <typename...> class Template>
+concept TemplateInstance = requires(T x) {
+  // Can we call a function that takes a range with
+  // T? If so, T is derived from a Range.
+  []<typename... args>(Template<args...> &) {}(x);
+};
+// clang-format on
 
 template <typename T>
-concept LBType = requires(T x) {
-                   { LowerBound{x} } -> std::same_as<T>;
-                 };
+concept LBType = TemplateInstance<T, LowerBound>;
 template <typename T>
-concept UBType = requires(T x) {
-                   { UpperBound{x} } -> std::same_as<T>;
-                 };
+concept UBType = TemplateInstance<T, UpperBound>;
 
-template <LBType L, UBType U> struct Range;
-
-template <typename T>
-concept RangeType = requires(T x) {
-                      { true } -> std::convertible_to<bool>;
-                    };
-
-template <typename B, bool is_open> struct LowerBound {
+template <typename B, typename is_open> struct LowerBound {
   static constexpr double lower_bound = B::lower_bound;
-  static constexpr bool lower_open = is_open;
+  static constexpr bool lower_open = open_trait<is_open>::value;
   static constexpr bool below(double val) {
     return lower_open ? lower_bound < val : lower_bound <= val;
   }
@@ -38,12 +46,12 @@ template <typename B, bool is_open> struct LowerBound {
     }
   }
 };
-template <typename B> using OpenLowerBound = LowerBound<B, true>;
-template <typename B> using ClosedLowerBound = LowerBound<B, false>;
+template <typename B> using OpenLowerBound = LowerBound<B, Open>;
+template <typename B> using ClosedLowerBound = LowerBound<B, Closed>;
 
-template <typename B, bool is_open> struct UpperBound {
+template <typename B, typename is_open> struct UpperBound {
   static constexpr double upper_bound = B::upper_bound;
-  static constexpr bool upper_open = is_open;
+  static constexpr bool upper_open = open_trait<is_open>::value;
   static constexpr bool above(double val) {
     return upper_open ? val < upper_bound : val <= upper_bound;
   }
@@ -55,8 +63,13 @@ template <typename B, bool is_open> struct UpperBound {
     }
   }
 };
-template <typename B> using OpenUpperBound = UpperBound<B, true>;
-template <typename B> using ClosedUpperBound = UpperBound<B, false>;
+template <typename B> using OpenUpperBound = UpperBound<B, Open>;
+template <typename B> using ClosedUpperBound = UpperBound<B, Closed>;
+
+template <LBType L, UBType U> struct Range;
+
+template <typename T>
+concept RangeType = TemplateInstance<T, Range>;
 
 template <LBType L, UBType U> struct Range {
   using lower = L;
